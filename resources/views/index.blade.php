195698a64b0e33778c60b20fd3f74ec5c3a8b8e6
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-11-06 16:51:13 +0100

    aggiunto "current series"

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,10 @@
 @section('pageContent')
     <section>
         <div class="container d-flex">
+            <div class="current-series">
+                <h2>CURRENT SERIES</h2>
+            </div>
+
             @foreach ($fumetti as $fumetto)
                 <div class="card">
                     <div class="img-card">
