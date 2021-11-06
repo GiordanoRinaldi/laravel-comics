a2a4a7f23310293a915a08185f20eae965ed4591
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-11-06 16:42:41 +0100

    inizio main, aggiunti fumetti

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,15 @@
 
 @section('pageContent')
     <section>
-        
+        <div class="container d-flex">
+            @foreach ($fumetti as $fumetto)
+                <div class="card">
+                    <div class="img-card">
+                        <img src="{{$fumetto["thumb"]}}" alt="{{$fumetto["series"]}}"> 
+                    </div>
+                    <span>{{$fumetto["series"]}}</span>
+                </div>
+            @endforeach
+        </div>
     </section>
 @endsection
