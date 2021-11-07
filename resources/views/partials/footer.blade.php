001a15a37c013855c0a2abc89fe525b625ad057c
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-11-07 23:27:46 +0100

    finito footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,58 +1,80 @@
 <footer>
-    <div class="bg-footer">
-        <div class="img-footer">
-            <img src="img/dc-logo-bg.png" alt="">
+    <div class="img-footer">
+        <div class="d-flex container footer">
+            <div class="column">
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li><a href="#">Character</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+                <h3>SHOP</h3>
+                <ul>
+                    <li><a href="#">Shop DC"</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+            <div class="column">
+                <h3>DC</h3>
+                <ul>
+                    <li><a href="#">Therms Of Use</a></li>
+                    <li><a href="#">Privacy policy (New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="column">
+                <h3>SITES</h3>
+                <ul>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>
+        </div>
+        <br>
+        <div class="container f-link">
+            <p>All site Content TM and &copy; DC Entertainment, unless othervise <a class="foot-links" href="#">noted here</a>. All rights reserved.</p>
+            <a class="foot-links" href="#">Cookies Settings</a>
         </div>
     </div>
-    <div class="d-flex container footer">
-        <div class="column">
-            <h3>DC COMICS</h3>
-            <ul>
-                <li><a href="#">Character</a></li>
-                <li><a href="#">Comics</a></li>
-                <li><a href="#">Movies</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">Games</a></li>
-                <li><a href="#">Videos</a></li>
-                <li><a href="#">News</a></li>
-            </ul>
-            <h3>SHOP</h3>
-            <ul>
-                <li><a href="#">Shop DC"</a></li>
-                <li><a href="#">Shop DC Collectibles</a></li>
-            </ul>
+    <div class="footer-bottom">
+        <div class="container">
+            <div>
+                <button class="btn-sign-up">SIGN-UP NOW!</button>
+            </div>
+            <div class="follow d-flex">
+                <h3>FOLLOW US</h3>
+                <div class="social">
+                    <img src="img/footer-facebook.png" alt="">
+                </div>
+                <div class="social">
+                    <img src="img/footer-twitter.png" alt="">
+                </div>
+                <div class="social">
+                    <img src="img/footer-youtube.png" alt="">
+                </div>
+                <div class="social">
+                    <img src="img/footer-pinterest.png" alt="">
+                </div>
+                <div class="social">
+                    <img src="img/footer-periscope.png" alt="">
+                </div>
+            </div>
         </div>
-        <div class="column">
-            <h3>DC</h3>
-            <ul>
-                <li><a href="#">Therms Of Use</a></li>
-                <li><a href="#">Privacy policy (New)</a></li>
-                <li><a href="#">Ad Choices</a></li>
-                <li><a href="#">Advertising</a></li>
-                <li><a href="#">Jobs</a></li>
-                <li><a href="#">Subscriptions</a></li>
-                <li><a href="#">Talent Workshops</a></li>
-                <li><a href="#">CPSC Certificates</a></li>
-                <li><a href="#">Ratings</a></li>
-                <li><a href="#">Shop Help</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
-        </div>
-        <div class="column">
-            <h3>SITES</h3>
-            <ul>
-                <li><a href="#">DC</a></li>
-                <li><a href="#">MAD Magazine</a></li>
-                <li><a href="#">DC Kids</a></li>
-                <li><a href="#">DC Universe</a></li>
-                <li><a href="#">DC Power Visa</a></li>
-            </ul>
-        </div>
-    </div>
-    <br>
-    <div class="container f-link">
-        <p>All site Content TM and &copy; DC Entertainment, unless othervise <a class="foot-links" href="#">noted here</a>. All rights reserved.</p>
-        <a class="foot-links" href="#">Cookies Settings</a>
     </div>
     
 </footer>
