0db421545b63c038be59e58e87138e79a1293fc4
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-11-07 12:58:10 +0100

    aggiunta parte finale main

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,6 +20,40 @@
         <div class="load-more">
             <button><strong>LOAD MORE</strong></button>
         </div>
+    </section>
+    <section class="logo-dc">
+        <div class="container d-flex">
+            <div>
+                <div class="img">
+                    <img src="img/buy-comics-digital-comics.png" alt="">
+                    <span>DIGITAL COMICS</span>
+                </div>
+            </div>
+            <div>
+                <div class="img">
+                    <img src="img/buy-comics-merchandise.png" alt="">
+                    <span>DC MERCHANDISE</span>
+                </div>
+            </div>
+            <div>
+                <div class="img">
+                    <img src="img/buy-comics-subscriptions.png" alt="">
+                    <span>SUBSCRIPTION</span>
+                </div>
+            </div>
+            <div>
+                <div class="img">
+                    <img src="img/buy-comics-shop-locator.png" alt="">
+                    <span>COMIC SHOP LOCATOR</span>
+                </div>
+            </div>
+            <div>
+                <div class="img last">
+                    <img src="img/buy-dc-power-visa.svg" alt="">
+                    <span>DC POWER VISA</span>
+                </div>
+            </div>
+        </div>
         
     </section>
 @endsection
